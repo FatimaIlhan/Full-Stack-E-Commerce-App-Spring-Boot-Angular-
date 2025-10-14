f053993cc877dcc2a3ed4b4bc64992fbf7d1048d
Fatima Hassan Noor <fatimahnoor05@gmail.com>
2025-10-14 09:59:00 +0200

    Initial project setup: Spring Boot REST API and DB connection configured

---
+++ ecommerce/src/main/java/com/ilhan/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,25 @@
+package com.ilhan.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+//Data known bug for OneToMany relationships
+@Getter
+@Setter
+public class ProductCategory {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id")
+    private  Long id;
+    @Column(name = "category_name")
+    private  String  categoryName;
+@OneToMany(cascade =  CascadeType.ALL, mappedBy = "category")
+@JsonManagedReference
+    private Set<Product> products;
+}
