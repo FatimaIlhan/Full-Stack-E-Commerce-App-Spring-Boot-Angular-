96ef42b25cdb5cf9deed1464196e607e0f717c8e
Fatima Hassan Noor <fatimahnoor05@gmail.com>
2025-10-18 17:07:15 +0200

    Update Angular component to subscribe to service data, display it in HTML, and add CORS support for Spring Boot app

--- 02 -Backend/ecommerce/ecommerce/src/main/java/com/ilhan/ecommerce/dao/ProductRepository.java
+++ 02 -Backend/ecommerce/ecommerce/src/main/java/com/ilhan/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ilhan.ecommerce.dao;
 
 import com.ilhan.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200/")
 public interface ProductRepository extends JpaRepository<Product , Long> {
 }
--- 03 -frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
+++ 03 -frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product-service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,6 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
-export class ProductList {
+export class ProductList implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+   this.listProducts();
+  }
+  listProducts() {
+    this.productService.getProductList().subscribe(data => {
+      this.products = data;
+    });
+  }
+  
+  // Action handlers used by the template. Keep simple for now.
+  viewProduct(product: Product): void {
+    // Replace with navigation or modal logic later
+    console.log('View product', product);
+  }
+
+  addToCart(product: Product): void {
+    // Replace with cart service integration later
+    console.log('Add to cart', product);
+  }
+
+
 
 }
