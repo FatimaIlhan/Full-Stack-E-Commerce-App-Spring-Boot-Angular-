a51aff71f6712cdd67860b2d97bdb23c4ac1bdc2
Fatima Hassan Noor <fatimahnoor05@gmail.com>
2025-10-18 15:42:12 +0200

    Add Product model and ProductService for API calls

--- 03 -frontend/angular-ecommerce/src/app/app.module.server.ts
+++ 03 -frontend/angular-ecommerce/src/app/app.module.server.ts
@@ -3,9 +3,14 @@ import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { App } from './app';
 import { AppModule } from './app-module';
 import { serverRoutes } from './app.routes.server';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
-  imports: [AppModule],
+  imports: [
+    AppModule,
+    HttpClientModule
+  ],
   providers: [provideServerRendering(withRoutes(serverRoutes))],
   bootstrap: [App],
 })
